8b8c1696e86ced22e90b4bb150b1782c8d297481
anOldSoul <984744192@qq.com>
2019-08-16 21:53:33 +0800

    admin

--- network-lock/src/components/common/NavMenu.vue
+++ network-lock/src/components/common/NavMenu.vue
@@ -6,37 +6,47 @@
           联网锁
         </div>
         <!-- 首页 -->
-        <el-menu-item index="1" @click="goView('/gateway')">
+        <el-menu-item index="1" @click="goView('/department')">
           <i class="el-icon-menu"></i>公寓管理
         </el-menu-item>
         <el-submenu index="2">
           <template slot="title">
             <i class="el-icon-k-manageorder"></i>设备管理
           </template>
-          <el-menu-item index="5-5" @click="goView('/')">门锁</el-menu-item>
-          <el-menu-item index="5-6" @click="goView('/gateway')">网关</el-menu-item>
+          <el-menu-item index="2-1" @click="goView('/')">门锁</el-menu-item>
+          <el-menu-item index="2-2" @click="goView('/gateway')">网关</el-menu-item>
         </el-submenu>
-        <el-menu-item index="1" @click="goView('/')">
+        <el-menu-item index="3" @click="goView('/')">
           <i class="el-icon-menu"></i>用户管理
         </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
+        <el-menu-item index="4" @click="goView('/')">
           <i class="el-icon-menu"></i>设备告警
         </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
-          <i class="el-icon-menu"></i>设备日志
-        </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
+        <el-submenu index="5">
+          <template slot="title">
+            <i class="el-icon-k-manageorder"></i>设备日志
+          </template>
+          <el-menu-item index="5-1" @click="goView('/deviceLog/deviceUnlock')">开锁记录</el-menu-item>
+          <el-menu-item index="5-2" @click="goView('/deviceLog/deviceWarning')">告警记录</el-menu-item>
+          <el-menu-item index="5-3" @click="goView('/deviceLog/deviceUserUpdate')">用户变更</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="6" @click="goView('/')">
           <i class="el-icon-menu"></i>数据统计
         </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
+        <el-menu-item index="7" @click="goView('/')">
           <i class="el-icon-menu"></i>公众号管理
         </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
+        <el-menu-item index="8" @click="goView('/')">
           <i class="el-icon-menu"></i>小程序管理
         </el-menu-item>
-        <el-menu-item index="1" @click="goView('/')">
-          <i class="el-icon-menu"></i>系统管理
-        </el-menu-item>
+        <el-submenu index="9">
+          <template slot="title">
+            <i class="el-icon-k-manageorder"></i>系统管理
+          </template>
+          <el-menu-item index="9-1" @click="goView('/system/adm')">管理员信息</el-menu-item>
+          <el-menu-item index="9-2" @click="goView('/system/role')">角色信息</el-menu-item>
+          <el-menu-item index="9-3" @click="goView('/system/log')">系统日志</el-menu-item>
+        </el-submenu>
       </el-menu>
     </el-col>
   </el-row>
--- network-lock/src/router/index.js
+++ network-lock/src/router/index.js
@@ -12,6 +12,14 @@ const LockDetail = resolve => require(['../views/lock/detail'], resolve)
 const Password = resolve => require(['../views/lock/password'], resolve)
 const GateWay = resolve => require(['../views/gateway/index'], resolve)
 const GateWayDetail = resolve => require(['../views/gateway/detail'], resolve)
+const Department = resolve => require(['../views/departmentManage/department'], resolve)
+const SystemAdm = resolve => require(['../views/system/adm/list'], resolve)
+const SystemLog = resolve => require(['../views/system/log/list'], resolve)
+const SystemRole = resolve => require(['../views/system/role/list'], resolve)
+const DeviceUnlock = resolve => require(['../views/deviceLog/unlock'], resolve)
+const DeviceUserUpdate = resolve => require(['../views/deviceLog/userUpdate'], resolve)
+const DeviceWarning = resolve => require(['../views/deviceLog/warning'], resolve)
+const UserManage = resolve => require(['../views/userManage/list'], resolve)
 
 const routers = [
   {
@@ -72,6 +80,114 @@ const routers = [
         text: '网关'
       }, {
         text: '列表'
+      }]
+    }
+  }, {
+    path: '/department',
+    name: 'Department',
+    component: Department,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '公寓管理'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/deviceLog/deviceUnlock',
+    name: 'DeviceUnlock',
+    component: DeviceUnlock,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '开锁记录'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/deviceLog/deviceWarning',
+    name: 'DeviceWarning',
+    component: DeviceWarning,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '告警记录'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/deviceLog/deviceUserUpdate',
+    name: 'DeviceUserUpdate',
+    component: DeviceUserUpdate,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '用户变更'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/system/log',
+    name: 'SystemLog',
+    component: SystemLog,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '系统日志'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/system/adm',
+    name: 'SystemAdm',
+    component: SystemAdm,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '管理员管理'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/system/role',
+    name: 'SystemRole',
+    component: SystemRole,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '角色管理'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/system/log',
+    name: 'SystemLog',
+    component: SystemLog,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '系统日志'
+      }, {
+        text: ''
+      }]
+    }
+  }, {
+    path: '/userManage',
+    name: 'UserManage',
+    component: UserManage,
+    meta: {
+      keepAlive: true,
+      breadcrumb: [{
+        text: '用户管理'
+      }, {
+        text: ''
       }]
     }
   }
