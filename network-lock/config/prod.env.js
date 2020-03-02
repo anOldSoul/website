5abc9861bb7ade5c74915e50216668d04105760a
anOldSoul <984744192@qq.com>
2020-03-02 09:38:03 +0800

    del network-lock

--- network-lock/config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_API: '"https://admin.openn.cn:8780"'
-}
--- network-lock/src/api/log.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function listLog(query) {
-  return request({
-    url: '/admin/log/queryByPage',
-    method: 'get',
-    params: query
-  })
-}
